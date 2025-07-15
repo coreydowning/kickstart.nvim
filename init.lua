dbb6d5dc40436fd8b88ae6f85ee55969fc86f596
Corey Downing <downing.c@gmail.com>
2025-07-14 18:50:32 -0600

    add quick states for keywords

--- init.lua
+++ init.lua
@@ -1003,7 +1003,7 @@ require('lazy').setup({
       require('orgmode').setup {
         org_agenda_files = '~/orgfiles/**/*',
         org_default_notes_file = '~/orgfiles/refile.org',
-        org_todo_keywords = { 'TODO', 'MAYBE', '|', 'DONE' },
+        org_todo_keywords = { 'MAYBE(m)', 'TODO(t)', '|', 'DONE(d)' },
       }
 
       -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
